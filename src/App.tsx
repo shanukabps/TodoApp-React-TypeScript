dead83d3f67fdcd69afcb2e18e02f6d83df980c6
hmsbps <bps95shanuka@gmail.com>
2021-01-30 18:57:25 +0530

    simple todo app for learn React with TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
 
 function App() {
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handelSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    //console.log("com", newTodos[index].complete);
+    newTodos[index].complete = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    //console.log("com", newTodos[index].complete);
+    newTodos.splice(index, 1); //a=[2,1,2,5,6]   a.splice[1,2]===> [1,5,6]
+    setTodos(newTodos);
+  };
+
+  console.log(todos);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Todo List</h1>
+      </div>
+      <form onSubmit={handelSubmit}>
+        <input
+          required
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      <section>
+        {todos.map((todo: ITodo, index: number) => {
+          return (
+            <div
+              key={index}
+              style={{ textDecoration: todo.complete ? "line-through" : "" }}
+            >
+              {todo.text}
+              {todo.complete}
+
+              <button type="button" onClick={() => completeTodo(index)}>
+                {todo.complete ? "Incomplete" : "complete"}
+              </button>
+              <button type="button" onClick={() => deleteTodo(index)}>
+                Delete
+              </button>
+            </div>
+          );
+        })}
+      </section>
     </div>
   );
 }
